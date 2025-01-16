3d7ab28da21f5a2df6c789d5df009c2ad3e50fee
q <imran.mamedov.03@gmail.com>
2025-01-16 06:36:13 +0400

    --allow-empty-message

--- cpu.sh
+++ cpu.sh
@@ -11,41 +11,41 @@ r_scale="configuration {font: \"Monocraft ${rofiScale}\";}"
 r_override="window {height: 1.7em; width: 8em; border: ${hypr_width}px; border-radius: 0;} element {border-radius: 0;}"
 
 show_freq_info() {
-    local message=$(sudo cpupower frequency-info -fm | grep -E 'current CPU frequency' | sed 's/current //; s/ frequency//; s/ (asserted by call to kernel)//')
-    notify-send "$message"
+        local message=$(sudo cpupower frequency-info -fm | grep -E 'current CPU frequency' | sed 's/current //; s/ frequency//; s/ (asserted by call to kernel)//')
+        notify-send "$message"
 }
 
 freq_range() {
-    [ "$1" -ge 400 ] && [ "$1" -le 4400 ]
+        [ "$1" -ge 400 ] && [ "$1" -le 4400 ]
 }
 
 freq=$(
-    rofi -p " CPU " -dmenu -theme-str "configuration {display-drun: \"Applications\";}" -theme-str "entry { placeholder: \" MHz\";}" -theme-str "${r_scale}" -theme-str "${r_override}" -config "${roconf}"
+        rofi -p " CPU " -dmenu -theme-str "configuration {display-drun: \"Applications\";}" -theme-str "entry { placeholder: \" MHz\";}" -theme-str "${r_scale}" -theme-str "${r_override}" -config "${roconf}"
 )
 if [ -n "$freq" ]; then
-    case "$freq" in
-        l)
-            echo "$LAYOUT"
-            notify-send -a "t3" -t 800 "${LAYOUT}"
-            ;;
-        i)
-            show_freq_info
-            ;;
-        min)
-            sudo cpupower frequency-set -u 400MHz >/dev/null 2>&1
-            show_freq_info
-            ;;
-        max)
-            sudo cpupower frequency-set -u 4000MHz >/dev/null 2>&1
-            show_freq_info
-            ;;
-        *)
-            if [[ "$freq" =~ ^[0-9]+$ ]] && freq_range "$freq"; then
-                sudo cpupower frequency-set -u "${freq}"MHz >/dev/null 2>&1
-                show_freq_info
-            else
-                notify-send "Invalid input. Please enter a numeric value between 400 and 4400 MHz."
-            fi
-            ;;
-    esac
+        case "$freq" in
+                l)
+                        echo "$LAYOUT"
+                        notify-send -a "t3" -t 800 "${LAYOUT}"
+                        ;;
+                i)
+                        show_freq_info
+                        ;;
+                min)
+                        sudo cpupower frequency-set -u 400MHz >/dev/null 2>&1
+                        show_freq_info
+                        ;;
+                max)
+                        sudo cpupower frequency-set -u 4000MHz >/dev/null 2>&1
+                        show_freq_info
+                        ;;
+                *)
+                        if [[ "$freq" =~ ^[0-9]+$ ]] && freq_range "$freq"; then
+                                sudo cpupower frequency-set -u "${freq}"MHz >/dev/null 2>&1
+                                show_freq_info
+                        else
+                                notify-send "Invalid input. Please enter a numeric value between 400 and 4400 MHz."
+                        fi
+                        ;;
+        esac
 fi
